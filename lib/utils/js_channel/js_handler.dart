8c21579e90e371002997d735a5d18c4a914b047b
zhangjianfu <451458765@qq.com>
2022-11-29 09:52:27 +0800

    feat：add js_handler_ble register

--- lib/utils/js_channel/js_handler.dart
+++ lib/utils/js_channel/js_handler.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_browser/utils/js_channel/js_handler_ble.dart';
 import 'package:flutter_browser/utils/js_channel/js_handler_push.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
@@ -10,5 +11,7 @@ class JsHandler {
   void addJavaScriptHandlers(InAppWebViewController controller) {
     // 注册推送相关JS Handler
     registerJsHandlerPush(controller);
+    // 注册蓝牙相关JS Handler
+    registerJsHandlerBle(controller);
   }
 }
